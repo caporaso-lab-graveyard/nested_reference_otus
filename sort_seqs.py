84b08bd3d2db92d3a4dfa445d837f7ada9db5f72
Jai Ram Rideout <jai.rideout@gmail.com>
2012-06-13 12:16:16 -0700

    Added new sort_seqs.py script for pre-sorting sequences by taxonomic depth and read length before OTU picking. Also added accompanying tests. More tests still need to be added and some more code needs to be pushed into functions.

---
+++ sort_seqs.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+from __future__ import division
+
+__author__ = "Jai Rideout"
+__copyright__ = "Copyright 2012, The QIIME project"
+__credits__ = ["Jai Ram Rideout"]
+__license__ = "GPL"
+__version__ = "1.5.0-dev"
+__maintainer__ = "Jai Ram Rideout"
+__email__ = "jai.rideout@gmail.com"
+__status__ = "Development"
+
+from operator import itemgetter
+from cogent.parse.fasta import MinimalFastaParser
+
+from qiime.parse import fields_to_dict
+from qiime.util import (parse_command_line_parameters,
+                        get_options_lookup,
+                        make_option)
+
+options_lookup = get_options_lookup()
+
+script_info = {}
+script_info['brief_description'] = """Sorts sequences by taxonomic depth and then by length"""
+script_info['script_description'] = """
+This script sorts a set of input sequences by taxonomic depth (i.e. the amount
+of taxonomic information available for a sequence) and by decreasing sequence
+read length for sequences that have the same taxonomic depth. This script may
+be used to presort sequences before picking OTUs to ensure that OTU centroids
+are "quality" sequences.
+"""
+script_info['script_usage'] = []
+script_info['script_usage'].append(("Sort sequences by taxonomic depth and "
+"length",
+"Sorts the sequences contained in the input FASTA file by taxonomic depth and "
+"then sorts sequences within each taxonomic depth by decreasing length so "
+"that longer reads come first.",
+"%prog -i input.fasta -t taxonomy_map.txt -o sorted_seqs.fasta"))
+script_info['output_description']= """
+The script creates a single output FASTA file containing the sorted sequences.
+"""
+
+script_info['required_options'] = [
+    options_lookup['fasta_as_primary_input'],
+    make_option('-t','--input_taxonomy_map',
+        help='the input taxonomy map file. This should be a tab-separated '
+        'file where each row contains a sequence ID and taxonomy string, '
+        'separated by a tab'),
+    options_lookup['output_fp']
+]
+script_info['optional_options'] = []
+script_info['version'] = __version__
+
+def compute_sequence_stats(fasta_lines, tax_map_lines, unknown_keywords=None):
+    seq_stats = {}
+
+    if tax_map_lines[0] != \
+            "ID Number\tGenBank Number\tNew Taxon String\tSource\n":
+        raise ValueError("The taxonomy map file appears to be invalid "
+                         "because it is either missing the header or has a "
+                         "corrupt header.")
+
+    for seq_id, seq_info in fields_to_dict(tax_map_lines[1:]).items():
+        if len(seq_info) != 3:
+            raise ValueError("The taxonomy map file appears to be invalid "
+                             "because it does not have exactly 4 columns.")
+        taxonomy = seq_info[1].split(';')
+
+        # Remove any 'unknown' taxonomy levels before computing the known
+        # taxonomy depth.
+        if unknown_keywords:
+            for unknown_keyword in unknown_keywords:
+                while unknown_keyword in taxonomy:
+                    taxonomy.remove(unknown_keyword)
+        seq_stats[seq_id] = [len(taxonomy)]
+
+    for seq_id, seq in MinimalFastaParser(fasta_lines):
+        if seq_id in seq_stats:
+            seq_stats[seq_id].extend([len(seq), seq])
+        else:
+            print ("Found sequence id %s in FASTA file that wasn't in the "
+                   "taxonomy mapping file\n" % seq_id)
+            # For now, assign a taxonomic depth of 0 because we don't have any
+            # taxonomic information for the sequence.
+            seq_stats[seq_id] = [0, len(seq), seq]
+    return seq_stats
+
+def main():
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
+
+    seq_stats = compute_sequence_stats(
+            open(opts.input_fasta_fp, 'U').readlines(),
+            open(opts.input_taxonomy_map, 'U').readlines(),
+            ['Incertae_sedis', 'unidentified'])
+    seq_stats_sorted = []
+    for seq_id, stats in seq_stats.items():
+        if len(stats) == 3:
+            seq_stats_sorted.append([seq_id] + stats)
+        else:
+            print ("Found sequence id %s in taxonomy mapping file that wasn't "
+                   "in FASTA file\n" % seq_id)
+
+    # Sort on sequence length (decreasing) first, then on taxonomic depth (also
+    # decreasing). sorted() is guaranteed to give us a stable sort.
+    seq_stats_sorted = sorted(seq_stats_sorted, key=itemgetter(2, 1),
+                              reverse=True)
+    out_fasta_f = open(opts.output_fp, 'w')
+    for seq in seq_stats_sorted:
+        out_fasta_f.write('>' + seq[0] + '\n' + seq[3] + '\n')
+    out_fasta_f.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_sort_seqs.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+from __future__ import division
+
+__author__ = "Jai Ram Rideout"
+__copyright__ = "Copyright 2012, The QIIME project"
+__credits__ = ["Jai Ram Rideout"]
+__license__ = "GPL"
+__version__ = "1.5.0-dev"
+__maintainer__ = "Jai Ram Rideout"
+__email__ = "jai.rideout@gmail.com"
+__status__ = "Development"
+
+"""Test suite for the sort_seqs.py module."""
+
+from cogent.util.unit_test import TestCase, main
+from sort_seqs import compute_sequence_stats
+
+class SortSeqsTests(TestCase):
+    """Tests for the sort_seqs.py module."""
+
+    def setUp(self):
+        """Define some sample data that will be used by the tests."""
+        self.fasta1 = [">1", "AGGT", ">2", "AGGA", ">3", "AGGC"]
+        self.tax_map1 = [
+                "ID Number\tGenBank Number\tNew Taxon String\tSource\n",
+                "1\tG1\tA;B;C\tfoo\n",
+                "2\tG2\tA;B;D\tfoo\n",
+                "3\tG3\tA;B;Z\tfoo\n"]
+        self.tax_map2 = [
+                "ID Number\tGenBank Number\tNew Taxon String\tSource\n",
+                "1\tG1\tA;B;C\tfoo\n",
+                "2\tG2\tA;B;D\tfoo\n",
+                "3\tG3\tA;B;Z;Z;F\tfoo\n"]
+        # Taxonomy string with no semicolons (only one level).
+        self.tax_map3 = [
+                "ID Number\tGenBank Number\tNew Taxon String\tSource\n",
+                "1\tG1\tA;B;C\tfoo\n",
+                "2\tG2\tA\tfoo\n",
+                "3\tG3\tA;B;Z;Z;F\tfoo\n"]
+        # Missing column in one of the rows.
+        self.tax_map_invalid1 = [
+                "ID Number\tGenBank Number\tNew Taxon String\tSource\n",
+                "1\tA;B;C\tfoo\n",
+                "2\tG2\tA;B;D\tfoo\n",
+                "3\tG3\tA;B;Z;Z;F\tfoo\n"]
+        # Missing header.
+        self.tax_map_invalid2 = [
+                "1\tG1\tA;B;C\tfoo\n",
+                "2\tG2\tA;B;D\tfoo\n",
+                "3\tG3\tA;B;Z;Z;F\tfoo\n"]
+
+    def test_compute_sequence_stats_unequal_depths(self):
+        """Test computing seq stats on unequal taxonomic depths."""
+        exp = {'1': [3, 4, 'AGGT'], '3': [2, 4, 'AGGC'], '2': [3, 4, 'AGGA']}
+        obs = compute_sequence_stats(self.fasta1, self.tax_map1, ['Z'])
+        self.assertEqual(obs, exp)
+
+    def test_compute_sequence_stats_single_depth(self):
+        """Test computing seq stats on taxonomy with only one level."""
+        exp = {'1': [3, 4, 'AGGT'], '3': [3, 4, 'AGGC'], '2': [1, 4, 'AGGA']}
+        obs = compute_sequence_stats(self.fasta1, self.tax_map3, ['Z'])
+        self.assertEqual(obs, exp)
+
+        exp = {'1': [2, 4, 'AGGT'], '3': [4, 4, 'AGGC'], '2': [0, 4, 'AGGA']}
+        obs = compute_sequence_stats(self.fasta1, self.tax_map3, ['A'])
+        self.assertEqual(obs, exp)
+
+    def test_compute_sequence_stats_multiple_unknown_keywords(self):
+        """Test computing seq stats on taxonomy with multiple unknown kws."""
+        exp = {'1': [3, 4, 'AGGT'], '3': [2, 4, 'AGGC'], '2': [3, 4, 'AGGA']}
+        obs = compute_sequence_stats(self.fasta1, self.tax_map2, ['Z', 'F'])
+        self.assertEqual(obs, exp)
+
+    def test_compute_sequence_stats_no_unknowns(self):
+        """Test computing seq stats on taxonomy with no unknown kws."""
+        exp = {'1': [3, 4, 'AGGT'], '2': [3, 4, 'AGGA'], '3': [5, 4, 'AGGC']}
+        obs = compute_sequence_stats(self.fasta1, self.tax_map2)
+        self.assertEqual(obs, exp)
+
+        obs = compute_sequence_stats(self.fasta1, self.tax_map2, [])
+        self.assertEqual(obs, exp)
+
+    def test_compute_sequence_stats_all_unknown(self):
+        """Test computing seq stats on taxonomy with all unknown tax levels."""
+        exp = {'1': [0, 4, 'AGGT'], '2': [0, 4, 'AGGA'], '3': [0, 4, 'AGGC']}
+        obs = compute_sequence_stats(self.fasta1, self.tax_map2, ['A', 'B',
+                'C', 'D', 'F', 'Z'])
+        self.assertEqual(obs, exp)
+
+    def test_compute_sequence_stats_invalid_tax_map(self):
+        """Test computing seq stats on invalid taxonomy maps."""
+        self.assertRaises(ValueError, compute_sequence_stats, self.fasta1,
+                          self.tax_map_invalid1, ['Z', 'F'])
+        self.assertRaises(ValueError, compute_sequence_stats, self.fasta1,
+                          self.tax_map_invalid2, ['Z', 'F'])
+
+
+if __name__ == "__main__":
+    main()
